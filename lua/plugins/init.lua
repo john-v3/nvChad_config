7c88dde685f848464638cbe00fb3a4e6b038b1ef
john-V3 <aasolidgaming@gmail.com>
2025-02-13 21:15:02 -0500

    get rid of lazy loading that vgit plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -19,7 +19,8 @@ return {
     -- or               , tag = 'v1.0.2',
     dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
     -- Lazy loading on 'VimEnter' event is necessary.
-    event = "VimEnter",
+    lazy = false,
+    -- event = "VimEnter",
     config = function()
       require("vgit").setup()
     end,
