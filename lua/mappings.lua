bc9a80fceb6131f6d590a163efb494a42beaf17c
BuildTools <unconfigured@null.spigotmc.org>
2025-03-25 19:55:36 -0400

    flash mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -31,6 +31,25 @@ map({ "n" }, "<leader>ft", "<cmd>OmniSharpFindType<CR>", { desc = "Find Type" })
 
 map({ "n" }, "<leader>fr", "<cmd>Telescope resume<CR>", { desc = "Find Type" })
 
+map({ "n", "x", "o" }, "s", function()
+  require("flash").jump()
+end, { desc = "Flash" })
+
+map({ "n", "x", "o" }, "S", function()
+  require("flash").treesitter()
+end, { desc = "Flash Treesitter" })
+
+map({ "o" }, "r", function()
+  require("flash").remote()
+end, { desc = "Remote Flash" })
+
+map({ "o", "x" }, "R", function()
+  require("flash").treesitter_search()
+end, { desc = "Treesitter Search" })
+
+map({ "c" }, "<c-s>", function()
+  require("flash").toggle()
+end, { desc = "Toggle Flash Search" })
 
 -- vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
 
