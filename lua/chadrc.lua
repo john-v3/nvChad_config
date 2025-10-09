cc0e0bf040b333e62f3397b5bf64f43b7e3d0070
john-V3 <aasolidgaming@gmail.com>
2025-10-09 00:54:35 -0400

    add mason presets

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,18 +1,21 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
+  theme = "onenord",
   transparency = true,
 
-	hl_override = {
-		Comment = { italic = true },
-		["@comment"] = { italic = true },
-	},
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
+  integrations = { "dap", "hop" },
 }
+
+M.mason = { pkgs = { "revive", "docker-language-server", "go-debug-adapter", "json-lsp", "lua-language-server", "markuplint", "omnisharp", "rust-analyzer" }, Skip = { }, }
 
 return M
