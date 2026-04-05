81ff9f4d3f47e4375c5fa23b529bc7c31423ed9a
john-V3 <aasolidgaming@gmail.com>
2026-04-05 02:11:36 -0400

    some small stuff

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,8 +1,23 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-vim.lsp.enable({ "html", "cssls", "gopls", "rust_analyzer", "nushell", "powershell_es", "sqls", "omnisharp", "jsonls", "xmlformatter", "angularls"}, true)
-vim.lsp.enable({"ts_ls"}, false)
+vim.lsp.enable( {
+    "html",
+    "cssls",
+    "gopls",
+    "rust_analyzer",
+    "nushell",
+    "powershell_es",
+    "sqls",
+    "omnisharp",
+    "jsonls",
+    "xmlformatter",
+    "angularls",
+    "docker_language_server",
+  },
+  true
+)
+vim.lsp.enable({ "ts_ls" }, false)
 
 vim.diagnostic.config {
   -- Use the default configuration
